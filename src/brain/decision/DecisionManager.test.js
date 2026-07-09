6add1260708f3314c26df7d4da06e8ba273a1663
orbisaideveloper <orbisaideveloper@gmail.com>
2026-07-10 01:18:21 +0530

    update

--- src/brain/decision/DecisionManager.test.js
+++
@@ -1,7 +0,0 @@
-import { DecisionManager } from './DecisionManager.js';
-
-test('manager works', async () => {
-  const manager = new DecisionManager();
-  const res = await manager.processRequest('CODING', 'Hi');
-  expect(res).toBeDefined();
-});
--- src/brain/orchestrator/ProviderOrchestrator.test.js
+++
@@ -1,9 +0,0 @@
-import { DecisionManager } from './DecisionManager.js';
-
-describe('DecisionManager', () => {
-  test('should process request', async () => {
-    const manager = new DecisionManager();
-    const result = await manager.processRequest('CODING', 'Test');
-    expect(result).toBeDefined();
-  });
-});
